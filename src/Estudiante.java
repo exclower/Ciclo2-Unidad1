7af3c960320538ba0631d33b636b244dcf29291e
exclower <exclower2@gmail.com>
2021-07-31 09:48:33 -0500

    Clase Estudiante

---
+++ src/Estudiante.java
@@ -0,0 +1,11 @@
+public class Estudiante {
+
+    String nombre;
+    public Estudiante(){
+        
+    }
+    public void estudiar(){
+        System.out.println("Estudiando");
+    }
+    
+}
---
+++ src/junio30.java
@@ -0,0 +1,9 @@
+public class junio30 {
+    public static void main(String[] args) {
+        Estudiante alexander = new Estudiante();
+        alexander.nombre = "Alexander Moreno";
+        System.out.println(alexander.nombre);
+        alexander.estudiar();
+    }
+    
+}
